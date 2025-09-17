0324a3c375f2e5fe74698012b98bbc025390c9bb
Zeddli <ruj8320@gmail.com>
2025-09-17 17:54:38 +0800

    frontend structure

---
+++ src/components/LoadingSatte.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LoadingSatte = () => {
+  return (
+    <div>LoadingSatte</div>
+  )
+}
+
+export default LoadingSatte
---
+++ src/components/NewCOntentButton.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NewCOntentButton = () => {
+  return (
+    <div>NewCOntentButton</div>
+  )
+}
+
+export default NewCOntentButton
